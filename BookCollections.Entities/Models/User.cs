00c4aff14b9b18fbc31c69cfc5420656c993caff
Bruno Mendes Campanati <bruno.campanate@al.infnet.edu.br>
2021-06-22 19:34:32 -0300

    Add entity models

---
+++ BookCollections.Entities/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookCollections.Entities.Models
+{
+    public class User
+    {
+        public User()
+        {
+            BookCollections = new HashSet<BookCollection>();
+        }
+
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<BookCollection> BookCollections { get; set; }
+    }
+}
