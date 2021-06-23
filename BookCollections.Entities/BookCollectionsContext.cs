b9084e1d34396a98b3a3cc0a0d0342a196b0863c
Bruno Mendes Campanati <bruno.campanate@al.infnet.edu.br>
2021-06-23 18:09:05 -0300

    Add database context

---
+++ BookCollections.Entities/BookCollectionsContext.cs
@@ -0,0 +1,27 @@
+﻿using BookCollections.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCollections.Entities
+{
+    public class BookCollectionsContext : DbContext
+    {
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<CollectionItem> CollectionItems { get; set; }
+        public virtual DbSet<BookCollection> BookCollections { get; set; }
+        private static bool _created = false;
+
+        public BookCollectionsContext()
+        {
+            if (!_created)
+            {
+                _created = true;
+                Database.EnsureDeleted();
+                Database.EnsureCreated();
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=book_collections.db");
+    }
+}
